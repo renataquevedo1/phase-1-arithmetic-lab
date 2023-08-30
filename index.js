4dd1965e56cc58ad6f9f280df71e1ad63e3c4a59
Renata Quevedo <renataq11@yahoo.com>
2023-08-30 08:43:31 +0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+const num1=2
+const num2=31
 
-// Write your code here
+
+const multiply=Number(num1)*Number(num2)
+console.log(multiply)
+
+const random= parseInt(Math.random(100)*100)
+
+const num3=28
+const num4=6
+const mod=Number(num3)%Number(num4)
+console.log(mod)
+
+    let sets=[0, 5, 10, 15, 20]
+    let nums=''
+
+    console.log(...sets)
+
+    let max=Math.max(...sets)
+    console.log(max)
